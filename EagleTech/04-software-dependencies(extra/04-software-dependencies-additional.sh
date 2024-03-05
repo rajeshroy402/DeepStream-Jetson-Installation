655f17e58e90f2eaa2a7426a3a095f32b3e62ad2
Rajesh Roy <rajeshroy402@gmail.com>
2024-03-05 12:16:51 +0530

    Create 04-software-dependencies-additional.sh

---
+++ EagleTech/04-software-dependencies(extra/04-software-dependencies-additional.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+#This is created by Rajesh Roy (rajeshroy402@gmail.com)
+
+############################################################
+
+sudo apt update -y
+sudo apt install python3-pip -y
+sudo apt-get install python3-dev default-libmysqlclient-dev build-essential -y
+sudo pip3 uninstall mysql-connector && pip3 install mysql-connector
+sudo apt-get install mysql-server -y
+sudo pip3 install mysql-connector 
+sudo pip3 install mysql-connector-python
+sudo pip3 install mysqlclient
+sudo pip3 uninstall boto3 && pip3 uninstall boto3
+sudo pip3 install boto3
+sudo pip3 uninstall psutil && pip3 uninstall psutil
+sudo pip3 install psutil
+sudo pip3 install boto3 botocore awscli --ignore-installed
